7ee96d97616231439caca4f24fe4b65f797adb50
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-28 21:52:59 +0100

    Add uploadthing styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config = {
@@ -82,4 +83,7 @@ const config = {
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
-export default config;
+export default withUt({
+  config,
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+});
