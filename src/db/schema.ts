a2ec446b6f484b58e3aa4022698fba1e68883edd
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-28 22:41:50 +0100

    define db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,12 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const products = sqliteTable("products", {
   id: text("id").primaryKey().notNull(),
   title: text("title").notNull(),
+  slug: text("slug").notNull().unique(),
+  price: real("price").notNull(),
+  description: text("description").notNull(),
+  category: text("category").notNull(),
+  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
 });
