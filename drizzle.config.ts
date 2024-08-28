80eda16fcf2d9fb0dad981fb3af737a3f5231742
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-28 22:54:07 +0100

    Migrate db

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
+  dialect: "sqlite", // 'postgresql' | 'mysql' | 'sqlite'
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
+  },
+});
