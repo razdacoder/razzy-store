a21f874bea427bb860dd052614b6e489117b999e
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-29 22:38:41 +0100

    Add orderby to product list api

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,11 @@
 import { db } from "@/db";
 import { products } from "@/db/schema";
+import { desc } from "drizzle-orm";
 
 export async function GET(request: Request) {
-  const data = await db.select().from(products);
+  const data = await db
+    .select()
+    .from(products)
+    .orderBy(desc(products.createdAt));
   return Response.json(data);
 }
