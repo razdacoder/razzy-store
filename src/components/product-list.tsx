b1077beabe5f50301fcbe6d003e9c3c2413cbd1c
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-29 22:36:50 +0100

    fix product list bug

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -35,14 +35,14 @@ export default function ProductList() {
       </main>
     );
   }
-
-  return (
-    <main className="py-4 mb-10">
-      <div className="px-4 md:container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
-        {data?.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </main>
-  );
+  if (data)
+    return (
+      <main className="py-4 mb-10">
+        <div className="px-4 md:container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-8">
+          {data?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </main>
+    );
 }
