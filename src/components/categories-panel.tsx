d46237ec16c17eb242ac90b85e0f8a77816fd6ae
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-30 01:17:23 +0100

    Update filter to use price and sort

--- src/components/categories-panel.tsx
+++ src/components/categories-panel.tsx
@@ -12,7 +12,7 @@ import { categories } from "@/lib/config";
 import { SlidersHorizontal } from "lucide-react";
 import { Button } from "./ui/button";
 
-import { cn } from "@/lib/utils";
+import { cn, formatToNaira } from "@/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 import {
@@ -36,6 +36,14 @@ export default function CategoriesPanel() {
   const pathname = usePathname();
   const router = useRouter();
   const categoryParam = searchParams.get("category");
+  const [isOpen, setOpen] = useState(false);
+  const sort = searchParams.get("sort");
+  const price = searchParams.get("price");
+  const [filters, setFilters] = useState({
+    sort: sort,
+    category: categoryParam,
+    price: price,
+  });
   const [range, setRange] = useState([0, 24]);
 
   const handleRangeChange = (value: number[]) => {
@@ -51,6 +59,31 @@ export default function CategoriesPanel() {
     }
     router.push(`${pathname}?${params.toString()}`);
   };
+
+  const handleApplyFilter = () => {
+    const params = new URLSearchParams(searchParams);
+    if (filters.category) {
+      params.set("category", filters.category);
+    } else {
+      params.delete("category");
+    }
+
+    if (filters.sort) {
+      params.set("sort", filters.sort);
+    } else {
+      params.delete("sort");
+    }
+
+    if (filters.price) {
+      params.set("price", filters.price.toString());
+    } else {
+      params.delete("price");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+    setOpen(false);
+  };
+
   return (
     <div className="py-2 border-b">
       <div className="px-4 md:container ">
@@ -106,7 +139,7 @@ export default function CategoriesPanel() {
               </Button>
             ))}
           </div>
-          <Sheet>
+          <Sheet open={isOpen} onOpenChange={setOpen}>
             <SheetTrigger asChild>
               <Button variant="outline" className="flex gap-2 items-center">
                 <SlidersHorizontal className="size-4" />
@@ -124,7 +157,13 @@ export default function CategoriesPanel() {
                       SORT BY
                     </AccordionTrigger>
                     <AccordionContent className="">
-                      <RadioGroup defaultValue="newset" className="space-y-2">
+                      <RadioGroup
+                        onValueChange={(value) =>
+                          setFilters((filters) => ({ ...filters, sort: value }))
+                        }
+                        defaultValue={!filters.sort ? "newset" : filters.sort}
+                        className="space-y-2"
+                      >
                         <div className="flex items-center space-x-2">
                           <RadioGroupItem
                             className="rounded-none border-2"
@@ -153,17 +192,76 @@ export default function CategoriesPanel() {
                     </AccordionContent>
                   </AccordionItem>
 
+                  <AccordionItem value="item-2">
+                    <AccordionTrigger className="text-sm hover:no-underline">
+                      CATEGORIES
+                    </AccordionTrigger>
+                    <AccordionContent className="">
+                      <RadioGroup
+                        defaultValue={
+                          !filters.category ? "all" : filters.category
+                        }
+                        onValueChange={(value) =>
+                          setFilters((filters) => ({
+                            ...filters,
+                            category: value,
+                          }))
+                        }
+                        className="space-y-2"
+                      >
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem
+                            className="rounded-none border-2"
+                            value="all"
+                            id="all"
+                          />
+                          <Label htmlFor="all" className="capitalize">
+                            All
+                          </Label>
+                        </div>
+                        {categories.map((category) => (
+                          <div
+                            key={`category-filter-${category}`}
+                            className="flex items-center space-x-2"
+                          >
+                            <RadioGroupItem
+                              className="rounded-none border-2"
+                              value={category}
+                              id={`id-${category}`}
+                            />
+                            <Label
+                              htmlFor={`id-${category}`}
+                              className="capitalize"
+                            >
+                              {category}
+                            </Label>
+                          </div>
+                        ))}
+                      </RadioGroup>
+                    </AccordionContent>
+                  </AccordionItem>
+
                   <AccordionItem value="item-3">
                     <AccordionTrigger className="text-sm hover:no-underline">
                       PRICE
                     </AccordionTrigger>
                     <AccordionContent className="my-4">
                       <Slider
-                        defaultValue={[50]}
-                        max={100}
-                        step={1}
-                        className=""
+                        defaultValue={
+                          !filters.price ? [100000] : [Number(filters.price)]
+                        }
+                        max={200000}
+                        step={1000}
+                        onValueChange={(value) =>
+                          setFilters((filters) => ({
+                            ...filters,
+                            price: value[0].toString(),
+                          }))
+                        }
                       />
+                      <p className="mt-2 text">
+                        {formatToNaira(Number(filters.price) || 100000)}
+                      </p>
                     </AccordionContent>
                   </AccordionItem>
                 </Accordion>
@@ -171,7 +269,11 @@ export default function CategoriesPanel() {
                   <Button variant="outline" size="lg" className="w-full">
                     RESST
                   </Button>
-                  <Button size="lg" className="w-full">
+                  <Button
+                    onClick={handleApplyFilter}
+                    size="lg"
+                    className="w-full"
+                  >
                     APPLY
                   </Button>
                 </SheetFooter>
