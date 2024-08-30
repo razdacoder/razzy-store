7636d86bd45fa4aa1b3a81ce102c91732e1a6498
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-30 12:38:17 +0100

    Update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Image
-      src="/logo-black.svg"
+      src="/razzy-store-logo.png"
       alt="Logo"
       width={100}
       height={100}
