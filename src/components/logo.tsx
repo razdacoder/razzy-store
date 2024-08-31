99b4f6e26900c02502e6f699ae2544f64f62613d
Ramon Rasheed <ramonrash2@gmail.com>
2024-08-31 15:03:28 +0100

    update logo to be link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      src="/razzy-store-logo.png"
-      alt="Logo"
-      width={100}
-      height={100}
-      className="w-[75px] md:w-[100px]"
-    />
+    <Link href="/">
+      <Image
+        src="/razzy-store-logo.png"
+        alt="Logo"
+        width={100}
+        height={100}
+        className="w-[75px] md:w-[100px]"
+      />
+    </Link>
   );
 }
